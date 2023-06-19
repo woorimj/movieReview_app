c78c60681f5a8233cc463c147e9749ffb4a6f32f
woorimj <forestwr06@gmail.com>
2023-06-19 16:12:22 +0900

    git commit

---
+++ app/src/main/java/dduwcom/mobile/finalreport/MainActivity.kt
@@ -0,0 +1,16 @@
+// 과제명: 영화 정보 관리 앱
+// 분반: 01분반
+// 학번,이름: 20211694 장우림
+// 제출일: 2023.6.23 금요일
+
+package dduwcom.mobile.finalreport
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
